59cca9ffd31cb65946f83d972d4c20809f631de3
Arthur <arthur.garnier1090@gmail.com>
2025-10-23 00:18:23 +0200

    update: add font and pages

---
+++ src/app/about/page.tsx
@@ -0,0 +1,7 @@
+export default function AboutPage() {
+  return (
+    <div className="min-h-screen w-full flex items-center justify-center bg-zinc-200">
+      <h1 className="text-4xl font-bold text-zinc-900">About Page</h1>
+    </div>
+  );
+}
---
+++ src/app/project/page.tsx
@@ -0,0 +1,7 @@
+export default function ProjectPage() {
+  return (
+    <div className="min-h-screen w-full flex items-center justify-center bg-zinc-200">
+      <h1 className="text-4xl font-bold text-zinc-900">Project Page</h1>
+    </div>
+  );
+}
